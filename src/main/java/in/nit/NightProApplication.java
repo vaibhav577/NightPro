677522c640583b3d0a91d843adf0661263c07240
vaibh <vaibh@DESKTOP-FC3V7KP>
2020-12-02 01:29:55 +0530

    this comments is for all file

---
+++ src/main/java/in/nit/NightProApplication.java
@@ -0,0 +1,13 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightProApplication.class, args);
+	}
+
+}
